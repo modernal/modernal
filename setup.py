ef2313684f9481d8f90262f68fea50cbc3ac34de
Szabolcs Dombi <cprogrammer1994@gmail.com>
2019-08-03 21:25:59 +0300

    add setup script and manifest file

--- setup.py
+++ setup.py
@@ -1,14 +1,82 @@
+import platform
+import re
+import sys
+
 from setuptools import Extension, setup
 
-ext = Extension(
+if sys.version_info < (3, 0):
+    raise Exception('Python 2 is not supported!')
+
+PLATFORMS = {'windows', 'linux', 'darwin', 'cygwin'}
+
+target = platform.system().lower()
+
+for known in PLATFORMS:
+    if target.startswith(known):
+        target = known
+
+if target not in PLATFORMS:
+    target = 'linux'
+
+if target == 'darwin':
+    import os
+    from distutils.sysconfig import get_config_var
+    from distutils.version import LooseVersion
+    if 'MACOSX_DEPLOYMENT_TARGET' not in os.environ:
+        current_system = LooseVersion(platform.mac_ver()[0])
+        python_target = LooseVersion(get_config_var('MACOSX_DEPLOYMENT_TARGET'))
+        if python_target < '10.9' and current_system >= '10.9':
+            os.environ['MACOSX_DEPLOYMENT_TARGET'] = '10.9'
+
+libraries = {
+    'windows': [],
+    'linux': ['dl'],
+    'cygwin': [],
+    'darwin': [],
+}
+
+extra_compile_args = {
+    'windows': [],
+    'linux': [],
+    'cygwin': [],
+    'darwin': ['-Wno-deprecated-declarations'],
+}
+
+extra_linker_args = {
+    'windows': [],
+    'linux': [],
+    'cygwin': [],
+    'darwin': [],
+}
+
+modernal = Extension(
     name='modernal',
-    sources=['modernal/modernal.cpp'],
-    extra_compile_args=['-fpermissive'],
-    depends=['setup.py'],
+    define_macros=[],
+    libraries=libraries[target],
+    extra_compile_args=extra_compile_args[target],
+    extra_link_args=extra_linker_args[target],
+    sources=[
+        'modernal/modernal.cpp',
+    ],
+    depends=[
+        'modernal/openal.hpp',
+        'setup.py',
+    ],
 )
+
+short_description = 'ModernAL: OpenAL for Python 3'
+long_description = open('README.md').read()
 
 setup(
     name='modernal',
-    version='0.1.0',
-    ext_modules=[ext],
+    version='0.9.0',
+    description=short_description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/modernal/modernal',
+    author='Szabolcs Dombi',
+    author_email='cprogrammer1994@gmail.com',
+    license='MIT',
+    ext_modules=[modernal],
+    platforms=['any'],
 )
